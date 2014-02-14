fb2b8381ebb3b0bdb0a1db86bbb42d74d23a87ed
Seivan Heidari <seivan@kth.se>
2014-02-14 20:58:59 +0100

    Refactor multipart - try 2

--- MacTestsAndSample/Sample/SHAppDelegate.m
+++ MacTestsAndSample/Sample/SHAppDelegate.m
@@ -31,7 +31,14 @@
 
   
   NSURLSessionTask * task = [session bet_customTaskOnResource:@"post" requestHandler:^NSURLRequest *(NSMutableURLRequest *modifierRequest) {
-    modifierRequest.HTTPMethod = @"POSt";
+    modifierRequest.HTTPMethod = @"POST";
+    NSString *boundary = [self boundaryString];
+    [modifierRequest addValue:[NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary] forHTTPHeaderField:@"Content-Type"];
+    
+    NSData *fileData = [[NSImage imageNamed:@"sample_image"] TIFFRepresentation];
+    NSData *data = [self createBodyWithBoundary:boundary username:@"rob" password:@"password" data:fileData filename:@"theFileorSo.png"];
+    
+    [modifierRequest setHTTPBody:data];
     return modifierRequest;
   } completionHandler:^(NSURL *location, NSData *responseObjectData, NSHTTPURLResponse *HTTPURLResponse, NSURLSessionTask *task, NSError *error) {
     NSString * response = [[NSString alloc] initWithData:responseObjectData encoding:NSUTF8StringEncoding];
@@ -55,4 +62,61 @@
   
 }
 
+- (NSData *) createBodyWithBoundary:(NSString *)boundary username:(NSString*)username password:(NSString*)password data:(NSData*)data filename:(NSString *)filename
+{
+  NSMutableData *body = [NSMutableData data];
+  
+  if (data) {
+    //only send these methods when transferring data as well as username and password
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"file\"; filename=\"%@\"\r\n", filename] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n\r\n", [self mimeTypeForPath:filename]] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:data];
+  }
+  
+  [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"username\"\r\n\r\n%@", username] dataUsingEncoding:NSUTF8StringEncoding]];
+  
+  [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"password\"\r\n\r\n%@", password] dataUsingEncoding:NSUTF8StringEncoding]];
+  
+  [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  
+  return body;
+}
+- (NSString *)boundaryString
+{
+  // generate boundary string
+  //
+  // adapted from http://developer.apple.com/library/ios/#samplecode/SimpleURLConnections
+  
+  CFUUIDRef  uuid;
+  NSString  *uuidStr;
+  
+  uuid = CFUUIDCreate(NULL);
+  assert(uuid != NULL);
+  
+  uuidStr = CFBridgingRelease(CFUUIDCreateString(NULL, uuid));
+  assert(uuidStr != NULL);
+  
+  CFRelease(uuid);
+  
+  return [NSString stringWithFormat:@"Boundary-%@", uuidStr];
+}
+
+- (NSString *)mimeTypeForPath:(NSString *)path
+{
+  // get a mime type for an extension using MobileCoreServices.framework
+  
+  CFStringRef extension = (__bridge CFStringRef)[path pathExtension];
+  CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, extension, NULL);
+  assert(UTI != NULL);
+  
+  NSString *mimetype = CFBridgingRelease(UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassMIMEType));
+  assert(mimetype != NULL);
+  
+  CFRelease(UTI);
+  
+  return mimetype;
+}
 @end
